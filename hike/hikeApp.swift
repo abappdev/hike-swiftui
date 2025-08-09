cc84795a5ccf960b9fe5d4c62b95e568b2c82a6c
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2025-08-09 21:24:48 +0530

    Initial Commit

---
+++ hike/hikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikeApp.swift
+//  hike
+//
+//  Created by Abhishek Bhalerao on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct hikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
